ed3b37eb9e2d808da991f65f89dfd90bd2496971
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2024-01-10 23:44:48 -0800

    server and api referenced

--- server.js
+++ server.js
@@ -5,4 +5,24 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const API_KEY = "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"
+
+app.post('/completions', (req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorizations": `Bearer ${ API_KEY }`,
+        }
+    }
+    try {
+        fetch("https://api.openai.com/v1/chat/completions", options)
+
+
+    } catch(error) {
+
+        console.log(error)
+    }
+
+})
+
 app.listen(PORT, () => console.log('Server is running on PORT ' + PORT))
